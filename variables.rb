4caee4cdd14e42209f96bb98ce4ea59175ef00ce
Curtis Barnard <curtis.barnard.jr@gmail.com>
2022-09-26 11:41:46 -0700

    update to string interpolation

--- variables.rb
+++ variables.rb
@@ -1,5 +1,5 @@
 person_name = "Curtis"
 year_of_birth = "1988"
 
-puts "Hello my name is " + person_name + ". Nice to meet you!"
-puts "I was born in " + year_of_birth
+puts "Hello my name is #{person_name}. Nice to meet you!"
+puts "I was born in #{year_of_birth}"
