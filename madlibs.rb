04ccfbee22df8a988aa6a41d9b31a3a247c1d9b8
Curtis Barnard <curtis.barnard.jr@gmail.com>
2022-09-27 16:28:30 -0700

    create initial madlib game

---
+++ madlibs.rb
@@ -0,0 +1,10 @@
+puts "Enter an adjective"
+adj = gets.chomp 
+puts "Enter a plural noun"
+plural_noun = gets.chomp 
+puts "Enter a name"
+name = gets.chomp 
+
+puts "Roses are #{adj}"
+puts "#{plural_noun} are blue"
+puts "I love #{name}"
