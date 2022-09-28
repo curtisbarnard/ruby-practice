93852636830c05c4dc78d8153fe332596794846e
Curtis Barnard <curtis.barnard.jr@gmail.com>
2022-09-28 08:33:49 -0700

    add hashmap examples

---
+++ hash.rb
@@ -0,0 +1,17 @@
+states = {
+  "Oregon" => "OR",
+  "Idaho" => "ID",
+  "California" => "CA"
+}
+
+states2 = {
+  Oregon: 'OR',
+  Idaho: 'ID',
+  California: 'CA'
+}
+
+puts states2[:Oregon]
+
+states[5] = "Hello"
+
+puts states[5]
